59167f231ff084c98b174bf63ed0211856ff7fcc
Bernard Strong <bstrong71@gmail.com>
2017-08-16 16:34:42 -0400

    boilerplate setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express         = require("express");
+const path            = require("path");
+const mustacheExpress = require("mustache-express");
+const routes          = require("routes");
+const bodyParser      = require("body-parser");
+const expressValidator= require("express-validator");
+const app             = express();
+
+app.engine("mustache", mustacheExpress());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "mustache");
+app.set("layout", "layout");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator());
+
+
+
+app.use(routes);
+
+app.listen(3000, function() {
+  console.log("App is running on localhost:3000");
+});
