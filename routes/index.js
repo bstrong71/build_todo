c97e96682a3e0b5ef3a9f5367213e6a34c30f2c6
Bernard Strong <bstrong71@gmail.com>
2017-08-16 22:18:36 -0400

    complete

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express   = require("express");
 const router    = express.Router();
-const todos     = [{task: "Wash the car", complete: false, id: 1}, {task: "Buy groceries", complete: true, id: 2}, {task: "Mow the grass", complete: false, id: 3}];
-// const done      = [];
+const todos     = [{task: "Wash the car", complete: false, id: 1}, {task: "Wax on, wax off", complete: false, id: 2}, {task: "Mow the grass", complete: false, id: 3}, {task: "Paint the fence", complete: false, id: 4}, {task: "Sand the floor", complete: false, id: 5}];
+
 
 
 router.get("/", function(req, res) {
@@ -12,7 +12,7 @@ router.get("/complete/:task", function(req, res) {
   todos[req.params.task - 1].complete = true;
   res.redirect("/");
 })
-// router.post("/d")
+
 
 router.post("/", function(req, res) {
   let obj = {task: req.body.task, complete: false, id: todos.length + 1};
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.engine("mustache", mustacheExpress());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "mustache");
 
-
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator());
